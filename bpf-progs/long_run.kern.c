631d0adc00d7e5128d472517bc9220ae232c6eda
Sid Chintamaneni <sidchintamaneni@phobos.cs.vt.edu>
2024-09-18 23:14:45 +0000

    test modified to run on networking testbed

--- bpf-progs/long_run.kern.c
+++ bpf-progs/long_run.kern.c
@@ -2,6 +2,8 @@
 #include <linux/types.h>
 #include <bpf/bpf_helpers.h>
 
+#define LEFT_SHIFT 2
+
 static int runner3(void *ctx) {
     bpf_printk("runner: Inside 3 loop function\n");
 
@@ -10,7 +12,7 @@ static int runner3(void *ctx) {
 
 static int runner2(void *ctx) {
     bpf_printk("runner: Inside 2 loop function\n");
-    __u32 iter = (1<<7);
+    __u32 iter = (1<<LEFT_SHIFT);
 
     bpf_loop(iter, runner3, NULL, 0);
 
@@ -19,7 +21,7 @@ static int runner2(void *ctx) {
 
 static int runner(void *ctx) {
     bpf_printk("runner: Inside 1 loop function\n");
-    __u32 iter = (1<<7);
+    __u32 iter = (1<<LEFT_SHIFT);
 
     bpf_loop(iter, runner2, NULL, 0);
 
@@ -31,7 +33,7 @@ int trigger_helper_prog(void *ctx) {
 
     bpf_printk("trigger_syscall_prog: Test BPF Loop Program\n");
 
-    __u32 iter = (1<<7);
+    __u32 iter = (1<<LEFT_SHIFT);
 
     bpf_loop(iter, runner, NULL, 0);
 
